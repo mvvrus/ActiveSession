54e04f22cb97f0413fc4d0f316815b3b793f3b97
mvvrus <vmurav@yandex.ru>
2023-05-31 14:53:33 +0300

    Change IActiveSessionRunner signature

--- MVVrus.AspNetCore.ActiveSession/IActiveSessionRunner.cs
+++ MVVrus.AspNetCore.ActiveSession/IActiveSessionRunner.cs
@@ -12,9 +12,15 @@ namespace MVVrus.AspNetCore.ActiveSession
 
     public interface IActiveSessionRunner<TResult>:IActiveSessionRunner
     {
-        public const int BEGINNING = 0;
+        public const Int32 BEGINNING = 0;
+        public const Int32 DEFAULT_ADVANCE=0;
 
-        public ValueTask<ActiveSessionRunnerResult<TResult>> GetMore(Int32 StartPosition, CancellationToken token=default);
+        public ValueTask<ActiveSessionRunnerResult<TResult>> GetMore(Int32 StartPosition, Int32 Advance, CancellationToken token=default);
+        public ValueTask<ActiveSessionRunnerResult<TResult>> GetMore(Int32 StartPosition, CancellationToken token = default)
+        {
+            return GetMore(StartPosition, DEFAULT_ADVANCE, token);
+        }
+
         public ActiveSessionRunnerResult<TResult> GetAvailable(Int32 StartPosition);
     }
 }
--- MVVrus.AspNetCore.ActiveSession/IActiveSessionRunnerProxy.cs
+++ MVVrus.AspNetCore.ActiveSession/IActiveSessionRunnerProxy.cs
@@ -4,7 +4,7 @@
     {
         public Boolean IsLocal { get; }
         public ValueTask<ActiveSessionRunnerState> GetStateAsync();
-        public ValueTask<Int32> GetPosition();
+        public ValueTask<Int32> GetPositionAsync();
         public ValueTask<ActiveSessionRunnerResult<TResult>> GetAvailableAsync(Int32 StartPosition);
     }
 }
