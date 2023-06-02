2eb4f3f66bfd6c79cf056d2234bc3c073e6cbf1a
mvvrus <vmurav@yandex.ru>
2023-06-02 04:50:17 +0300

    Complete ActiveSessionBuilderExtensions XML documentation.

--- MVVrus.AspNetCore.ActiveSession/ActiveSessionBuilderExtensions.cs
+++ MVVrus.AspNetCore.ActiveSession/ActiveSessionBuilderExtensions.cs
@@ -8,10 +8,21 @@ namespace MVVrus.AspNetCore.ActiveSession
     public static class ActiveSessionBuilderExtensions
     {
         /// <summary>
-        /// TODO
+        /// Adds the ActiveSession midleware to an application middleware pipeline to be built
         /// </summary>
-        /// <param name="Builder"></param>
-        /// <returns></returns>
+        /// <param name="Builder">The application middleware pipeline builder</param>
+        /// <returns>The <paramref name="Builder"/> parameter value to allow call chaining.</returns>
+        /// <remarks>
+        /// <para>
+        /// The ActiveSession midleware adds an implementation of <see cref="IActiveSessionFeature"/> feature interface
+        /// to the <see cref="HttpContext.Features"/> collection.
+        /// </para>
+        /// <para>
+        /// The feature is added only if at list one runner factory service (<see cref="IActiveSessionRunnerFactory{TRequest, TResult}"/>) 
+        /// was registered inthe application's service container via one of AddActiveSession extension methods defined in the 
+        /// <see cref="ActiveSessionServiceCollectionExtensions"/> class.
+        /// </para>
+        /// </remarks>
         public static IApplicationBuilder UseActiveSessions(this IApplicationBuilder Builder) 
         {
             //Try to get IActiveSessionStore fro DI container to check if any of AddActiveSessions methods were ever called
