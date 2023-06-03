35a6bb135e693fa8adfc443c81a9e7f7644621a1
mvvrus <vmurav@yandex.ru>
2023-06-03 04:11:17 +0300

    Add logging to the ActiveSessionMiddleware class.

--- MVVrus.AspNetCore.ActiveSession/Internal/ActiveSessionMiddleware.cs
+++ MVVrus.AspNetCore.ActiveSession/Internal/ActiveSessionMiddleware.cs
@@ -7,31 +7,66 @@ namespace MVVrus.AspNetCore.ActiveSession.Internal
     {
         readonly RequestDelegate _next;
         readonly IActiveSessionStore _store;
-        readonly ILogger _logger;
+        readonly ILogger? _logger;
 
-        public ActiveSessionMiddleware(RequestDelegate Next, IActiveSessionStore Store, ILoggerFactory LoggerFactory)
+        public ActiveSessionMiddleware(RequestDelegate Next, IActiveSessionStore Store, ILoggerFactory? LoggerFactory)
         {
-            _next=Next??throw new ArgumentNullException(nameof(Next));
-            _store=Store??throw new ArgumentNullException(nameof(Store));
-            if (LoggerFactory is null) throw new ArgumentNullException(nameof(LoggerFactory));
-            _logger = LoggerFactory.CreateLogger(LOGGING_CATEGORY_NAME); 
+            _logger=LoggerFactory?.CreateLogger(LOGGING_CATEGORY_NAME);
+#if TRACE
+            _logger?.LogTraceConstructActiveSessionMiddleware();
+#endif
+            try {
+                _next=Next??throw new ArgumentNullException(nameof(Next));
+                _store=Store??throw new ArgumentNullException(nameof(Store));
+                _logger?.LogDebugActiveSessionMiddlewareAdded();
+            }
+            catch (Exception exception) {
+                _logger?.LogErrorMiddlewareCannotBeCreated(exception);
+#if TRACE
+                _logger?.LogTraceConstructActiveSessionMiddlewareExit();
+#endif
+                throw;
+            }
+#if TRACE
+            _logger?.LogTraceConstructActiveSessionMiddlewareExit();
+#endif
         }
 
-        public async Task Invoke(HttpContext context)
+        public async Task Invoke(HttpContext Context)
         {
-            //TODO Check that Features.Get<ISessionFeature>() does return non-null?
-            IActiveSessionFeature feature = new ActiveSessionFeature(_store, context.Features.Get<ISessionFeature>()?.Session);
-            //TODO Implement logging
-            context.Features.Set(feature);
-            try
-            {
-                await _next(context);
+#if TRACE
+            _logger?.LogTraceInvokeActiveSessionMiddleware(Context.TraceIdentifier);
+#endif
+            IActiveSessionFeature feature = new ActiveSessionFeature(_store, Context.Features.Get<ISessionFeature>()?.Session);
+#if TRACE
+            _logger?.LogTraceActiveSessionMiddlewareFeatureCreated(Context.TraceIdentifier);
+#endif
+            Context.Features.Set(feature);
+            if(_logger!=null && _logger!.IsEnabled(LogLevel.Debug)) 
+                _logger!.LogDebugActiveSessionMiddlewareInvoked(Context.TraceIdentifier, Context.Session.Id);
+            try {
+#if TRACE
+                _logger?.LogTraceActiveSessionMiddlewareInvokeRest(Context.TraceIdentifier);
+#endif
+                await _next(Context);
+#if TRACE
+                _logger?.LogTraceActiveSessionMiddlewareControlReturns(Context.TraceIdentifier);
+#endif
                 await feature.CommitAsync();
             }
-            finally
-            {
-                context.Features.Set<IActiveSessionFeature>(null);
+            catch (Exception exception) {
+                _logger?.LogErrorPipelineException(exception, Context.TraceIdentifier);
+                throw;
+            }
+            finally {
+#if TRACE
+                _logger?.LogTraceActiveSessionMiddlewareCleanupStart(Context.TraceIdentifier);
+#endif
+                Context.Features.Set<IActiveSessionFeature>(null);
                 feature.Clear();
+#if TRACE
+                _logger?.LogTraceActiveSessionMiddlewareExit(Context.TraceIdentifier);
+#endif
             }
         }
     }
--- MVVrus.AspNetCore.ActiveSession/Internal/LoggingExtensions.cs
+++ MVVrus.AspNetCore.ActiveSession/Internal/LoggingExtensions.cs
@@ -4,8 +4,11 @@ namespace MVVrus.AspNetCore.ActiveSession.Internal
 {
     internal static partial class LoggingExtensions
     {
-        [LoggerMessage(1, Error, "")]
-        public static partial void LogError1(this ILogger Logger, Exception AnException);
+        [LoggerMessage(1, Error, "The exception occured while creating the ActiveSession middleware.")]
+        public static partial void LogErrorMiddlewareCannotBeCreated(this ILogger Logger, Exception AnException);
+
+        [LoggerMessage(2, Error, "The exception occured while the rest of the pipeline, TraceIdentifier=\"{TraceIdentifier}\".")]
+        public static partial void LogErrorPipelineException(this ILogger Logger, Exception AnException, String TraceIdentifier);
 
 
         [LoggerMessage(1000, Warning, "Cannot obtain an ActiveSession store service from the application service container. The ActiveSession middleware cannot be registered. Was the IServiceCollection.AddActiveServices extension method called at least once?")]
@@ -17,6 +20,9 @@ namespace MVVrus.AspNetCore.ActiveSession.Internal
 
         [LoggerMessage(3001, Debug, "ActiveSession middleware is added to the middleware pipeline.")]
         public static partial void LogDebugActiveSessionMiddlewareAdded(this ILogger Logger);
+
+        [LoggerMessage(3002, Debug, "ActiveSession middleware is invoked, TraceIdentifier=\"{TraceIdentifier}\", Session.Id=\"{SessionId}\".")]
+        public static partial void LogDebugActiveSessionMiddlewareInvoked(this ILogger Logger, String TraceIdentifier, String SessionId);
 
 
         [LoggerMessage(4000, Trace, "Enetering ActiveSessionBuilderExtensions.UseActiveSessions.")]
@@ -30,5 +36,23 @@ namespace MVVrus.AspNetCore.ActiveSession.Internal
 
         [LoggerMessage(4003, Trace, "Exiting ActiveSessionMiddleware constructor.")]
         public static partial void LogTraceConstructActiveSessionMiddlewareExit(this ILogger Logger);
+
+        [LoggerMessage(4010, Trace, "Entering ActiveSessionMiddleware, TraceIdentifier=\"{TraceIdentifier}\".")]
+        public static partial void LogTraceInvokeActiveSessionMiddleware(this ILogger Logger, String TraceIdentifier);
+
+        [LoggerMessage(4011, Trace, "A feature object is created, TraceIdentifier=\"{TraceIdentifier}\".")]
+        public static partial void LogTraceActiveSessionMiddlewareFeatureCreated(this ILogger Logger, String TraceIdentifier);
+
+        [LoggerMessage(4012, Trace, "Invocing the rest of the middleware pipeline after the ActiveSession middleware, TraceIdentifier=\"{TraceIdentifier}\".")]
+        public static partial void LogTraceActiveSessionMiddlewareInvokeRest(this ILogger Logger, String TraceIdentifier);
+
+        [LoggerMessage(4013, Trace, "Control from the rest of the pipeline was returned without exceptions, TraceIdentifier=\"{TraceIdentifier}\".")]
+        public static partial void LogTraceActiveSessionMiddlewareControlReturns(this ILogger Logger, String TraceIdentifier);
+
+        [LoggerMessage(4014, Trace, "Cleaning up the feature object, TraceIdentifier=\"{TraceIdentifier}\".")]
+        public static partial void LogTraceActiveSessionMiddlewareCleanupStart(this ILogger Logger, String TraceIdentifier);
+
+        [LoggerMessage(4015, Trace, "ActiveSession middleware exited, TraceIdentifier=\"{TraceIdentifier}\".")]
+        public static partial void LogTraceActiveSessionMiddlewareExit(this ILogger Logger, String TraceIdentifier);
     }
 }
