066a5a3cc2826d640e139cd202403a62fa3a7a51
mvvrus <vmurav@yandex.ru>
2023-06-22 19:55:17 +0300

    Continue to implement unit tests

---
+++ ActiveSession.Tests/ActiveSessionServiceCollectionExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MVVrus.AspNetCore.ActiveSession;
+using MVVrus.AspNetCore.ActiveSession.Internal;
+using System.Collections.ObjectModel;
+
+namespace ActiveSession.Tests
+{
+    public class ActiveSessionServiceCollectionExtensionsTests
+    {
+        public record Request1
+        {
+            public String Arg { get; set; } = "";
+        }
+
+        public class Result1
+        {
+            public String Value { get; init; }
+            public Result1(String Value)
+            {
+                this.Value=Value;
+            }
+        }
+
+        public class SpyRunner1 : SpyRunnerBase<Result1>
+        {
+            public Request1 Request { get; init; }
+            public SpyRunner1(Request1 Request)
+            {
+                this.Request=Request;
+            }
+        }
+
+
+        [Fact]
+        public void DelegateFactoryAddition()
+        {
+            IServiceCollection services = new ServiceCollection();
+            Func<Request1, IActiveSessionRunner<Result1>> factory = x => new SpyRunner1(x);
+            services.AddActiveSessions(factory);
+        }
+    }
+}
--- ActiveSession.Tests/TypeRunnerFactoryTest.cs
+++ ActiveSession.Tests/TypeRunnerFactoryTest.cs
@@ -1,10 +1,5 @@
 ﻿using MVVrus.AspNetCore.ActiveSession;
 using MVVrus.AspNetCore.ActiveSession.Internal;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ActiveSession.Tests
 {
