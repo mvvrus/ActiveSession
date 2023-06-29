ff5d5b54962a90f805cecd9251d1a8e3b8df336e
mvvrus <vmurav@yandex.ru>
2023-06-30 01:39:57 +0300

    Proceed developing EnumAdapterRunner class

--- MVVrus.AspNetCore.ActiveSession/EnumAdapterRunner.cs
+++ MVVrus.AspNetCore.ActiveSession/EnumAdapterRunner.cs
@@ -15,7 +15,7 @@ namespace MVVrus.AspNetCore.ActiveSession
         Int32 _busy;
         CancellationTokenSource _tokenSource;
         BlockingCollection<TResult> _queue;
-        volatile ActiveSessionRunnerState _state;
+        Int32 _state;
         Object _lock;
         const String PARALLELISM_NOT_ALLOWED= "Parallel operations are not allowed.";
 
@@ -33,9 +33,10 @@ namespace MVVrus.AspNetCore.ActiveSession
         public ActiveSessionRunnerState State { 
             get {
                 CheckDisposed();
-                if (_state==Stalled&&_queue.Count>0) 
+                ActiveSessionRunnerState state = (ActiveSessionRunnerState)Volatile.Read(ref _state);
+                if (state==Stalled&&_queue.Count>0) 
                     return Progressed;
-                else return _state; 
+                else return state; 
             } 
         }
 
@@ -46,8 +47,10 @@ namespace MVVrus.AspNetCore.ActiveSession
         public void Abort()
         {
             if (_disposed) return;
-            //TODO
-            throw new NotImplementedException();
+            //TODO lock?
+            Boolean discard_it=Interlocked.CompareExchange(ref _state, (Int32)Aborted, (Int32)Stalled)==(Int32)Stalled
+                || Interlocked.CompareExchange(ref _state, (Int32)Aborted, (Int32)NotStarted)==(Int32)NotStarted;
+            //TODO Signal completion
         }
 
         /// <inheritdoc/>
@@ -63,19 +66,31 @@ namespace MVVrus.AspNetCore.ActiveSession
         }
 
         /// <inheritdoc/>
-        public ActiveSessionRunnerResult<IEnumerable<TResult>> GetAvailable(Int32 StartPosition, String? TraceIdentifier=null)
+        public ActiveSessionRunnerResult<IEnumerable<TResult>> GetAvailable(Int32 StartPosition, Int32 Advance = MAXIMUM_ADVANCE, String? TraceIdentifier=null)
         {
             CheckDisposed();
-            if(Interlocked.CompareExchange(ref _busy, 1, 0)!=0) {
+            ActiveSessionRunnerResult<IEnumerable<TResult>> result = default;
+            if (Interlocked.CompareExchange(ref _busy, 1, 0)!=0) {
                 ThrowInvalidParallelism();
             }
             try {
-                //TODO
-                throw new NotImplementedException();
+                List<TResult> result_list = new List<TResult>();
+                for(Int32 i=0;i<Advance && _queue.Count>0 && Volatile.Read(ref _state)==(Int32)Stalled; i++) {
+                    TResult? item;
+                    if (_queue.TryTake(out item)) {
+                        result_list.Add(item??throw new NullReferenceException());
+                        Position+=1;
+                    }
+                }
+                if (_queue.IsAddingCompleted && _queue.Count==0) {
+                    //TODO Change State to Completed,
+                }
+                result=new ActiveSessionRunnerResult<IEnumerable<TResult>>(result_list, State, Position);
             }
             finally {
                 Volatile.Write(ref _busy, 0);
             }
+            return result;
         }
 
         /// <inheritdoc/>
@@ -86,7 +101,12 @@ namespace MVVrus.AspNetCore.ActiveSession
         }
 
         /// <inheritdoc/>
-        public async ValueTask<ActiveSessionRunnerResult<IEnumerable<TResult>>> GetMoreAsync(Int32 StartPosition, Int32 Advance, String? TraceIdentifier = null, CancellationToken token = default)
+        public async ValueTask<ActiveSessionRunnerResult<IEnumerable<TResult>>> GetMoreAsync(
+            Int32 StartPosition, 
+            Int32 Advance, 
+            String? TraceIdentifier = null, 
+            CancellationToken Token = default
+        )
         {
             CheckDisposed();
             ActiveSessionRunnerResult<IEnumerable<TResult>> result=default;
@@ -100,16 +120,14 @@ namespace MVVrus.AspNetCore.ActiveSession
                 StartSourceEnumerationIfNotStarted();
                 List<TResult> result_list = new List<TResult>();
 
-                for ( int i=0; i<Advance && _state==Stalled;i++) {
-                    //TODO Check for cancellation
+                for ( int i=0; i<Advance &&Volatile.Read(ref _state)==(Int32)Stalled && !Token.IsCancellationRequested;i++) {
                     TResult? item;
                     if (_queue.TryTake(out item)) {
                         result_list.Add(item??throw new NullReferenceException());
                         Position+=1;
-                        //TODO
                     }
                     else if(_queue.IsAddingCompleted) {
-                        //TODO
+                        //TODO Signal completion
                     }
                     else await this; //TODO Log trace
                 }
@@ -126,10 +144,12 @@ namespace MVVrus.AspNetCore.ActiveSession
             if (_disposed) throw new ObjectDisposedException(this.GetType().FullName!);
         }
 
-        Action? _continuation;
+        #region Await stuff
+        WaitCallback? _continuation_callback;
+        ManualResetEventSlim _complete_event = new ManualResetEventSlim(true);
         public EnumAdapterRunner<TResult> GetAwaiter() { return this; }
-        public Boolean IsCompleted { get; private set; }
-        public void GetResult() {}
+        public Boolean IsCompleted { get {return _queue.Count>0;} }
+        public void GetResult() { _complete_event.Wait(); }
 
         public void OnCompleted(Action continuation)
         { 
@@ -143,10 +163,27 @@ namespace MVVrus.AspNetCore.ActiveSession
 
         private void Schedule(Action continuation)
         {
-            if(Interlocked.CompareExchange(ref _continuation, continuation, null)!=null) {
-                ThrowInvalidParallelism();
+            WaitCallback wait_callback = (_ => { _complete_event.Set(); continuation(); });
+            _complete_event.Reset();
+            try {
+                if(Interlocked.CompareExchange(ref _continuation_callback, wait_callback, null)!=null) {
+                    ThrowInvalidParallelism();
+                }
+            }
+            catch {
+                _complete_event.Set();
+            }
+            if (_queue.IsAddingCompleted) TryRunAwaitContinuation();
+        }
+
+        private void TryRunAwaitContinuation()
+        {
+            WaitCallback? continuation_callback = Interlocked.Exchange(ref _continuation_callback, null);
+            if (continuation_callback!=null) {
+                ThreadPool.QueueUserWorkItem(continuation_callback!);
             }
         }
+        #endregion
 
         void ThrowInvalidParallelism()
         {
@@ -157,25 +194,32 @@ namespace MVVrus.AspNetCore.ActiveSession
         void StartSourceEnumerationIfNotStarted()
         {
             if (State!=NotStarted) return;
-            _state=Stalled;
+            _state=(Int32)Stalled;
             //TODO Implement cancellation
             Task.Run(EnumerateSource);
         }
 
         void EnumerateSource()
         {
-            foreach (TResult item in _base) {
-                //TODO Process Abort() call i.e. implement cancellation
-                //TODO Add item to the queue
-                _queue.Add(item);
-                Action? continuation = Volatile.Read(ref _continuation);
-                if (continuation!=null) {
-                    Volatile.Write(ref _continuation, null);
-                    ThreadPool.QueueUserWorkItem(_=>continuation!());
+            try {
+                foreach (TResult item in _base) {
+                    if ((Int32)_state<0) //i.e. State==Aborted || State==Failed
+                        break;
+                    //TODO Add item to the queue
+                    _queue.Add(item);
+                    TryRunAwaitContinuation();
                 }
+                //TODO Process completion
             }
-            //TODO Process completion
-            _queue.CompleteAdding();
+            catch {
+                //TODO Set _state=Failed (and re-throw an exception?)
+                throw;
+            }
+            finally {
+                _queue.CompleteAdding();
+                //if(_state>=0) ?
+                TryRunAwaitContinuation();
+            }
         }
         
     }
