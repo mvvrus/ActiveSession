bd8b2608b76c09c0e8b51cb49477b23ffdd89e98
mvvrus <vmurav@yandex.ru>
2023-08-03 03:37:02 +0300

    Implement SimpleRunnerHandler in ProbeApp

--- ProbeApp/SimpleRunnerHandler.cs
+++ ProbeApp/SimpleRunnerHandler.cs
@@ -4,14 +4,29 @@ namespace ProbeApp
 {
     public static class SimpleRunnerHandler
     {
-        public static async Task<string> PageHandler(HttpContext context, int? RunnerNumber)
+        public static async Task<string> PageHandler(HttpContext Context, int? RunnerNumber)
         {
-            IActiveSession? active_session = context.Features.Get<IActiveSessionFeature>()?.ActiveSession;
+            IActiveSession? active_session = Context.Features.Get<IActiveSessionFeature>()?.ActiveSession;
             SimpleRunnerParams runner_params= new SimpleRunnerParams {Immediate=10, End=100};
             String id = active_session?.Id??"<null>";
-            (IActiveSessionRunner<int> runner, int runner_key) = active_session?.CreateRunner<SimpleRunnerParams,int>(runner_params, context.TraceIdentifier)??default;
+            int runner_key=-1;
+            IActiveSessionRunner<int>? runner=null;
+            if (RunnerNumber.HasValue) {
+                runner_key=RunnerNumber.Value;
+                runner=active_session?.GetRunner<int>(runner_key, Context);
+            }
+            if(runner==null)
+                (runner, runner_key) = active_session?.CreateRunner<SimpleRunnerParams,int>(runner_params, Context)??default;
             ActiveSessionRunnerResult<int> runner_result = await runner.GetMoreAsync(-1);
-            String result = $"Simple ActiveSession runner handler. ActiveSession.Id={id}, Runner key ={runner_key}, Runner result={runner_result}";
+            String result;
+            String head = "";
+            String body = "";
+
+            body+=$"Simple ActiveSession runner handler. ActiveSession.Id={id}, Runner key={runner_key}" +
+                $"<BR/> Runner result={runner_result}";
+            body+=$"<BR/> <A href=\"/SimpleRunner?RunnerNumber={runner_key}\">Proceed with this runner</A>";
+            Context.Response.ContentType="text/html";
+            result="<!DOCTYPE HTML><HTML><HEAD>"+head+"</HEAD><BODY>"+body+"</BODY></HTML>";
             return result;
         }
     }
