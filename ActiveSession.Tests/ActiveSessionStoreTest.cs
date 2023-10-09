ae87720a1ce4fcf73d4cca1c253e00e194b0362c
mvvrus <vmurav@yandex.ru>
2023-10-09 05:49:44 +0300

    implementing and performing ActiveSessionStore class test
    Begin implementing the CreateOrFetchSession method test

--- ActiveSession.Tests/ActiveSessionStoreTest.cs
+++ ActiveSession.Tests/ActiveSessionStoreTest.cs
@@ -10,21 +10,25 @@ using System.Text;
 using System.Threading.Tasks;
 using MVVrus.AspNetCore.ActiveSession.Internal;
 using System.Linq.Expressions;
+using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.Http;
+using Active_Session = MVVrus.AspNetCore.ActiveSession.Internal.ActiveSession;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ActiveSession.Tests
 {
     public class ActiveSessionStoreTest
     {
         [Fact]
-        public void CreateActiveSessionStore() 
+        public void CreateActiveSessionStore()
         {
             ConstructorTestSetup ts;
-            Mock<IMemoryCache> dummy_cache=new Mock<IMemoryCache>();
+            Mock<IMemoryCache> dummy_cache = new Mock<IMemoryCache>();
             ts=new ConstructorTestSetup(dummy_cache);
 
             //Test case: null IMemoryCahe argument while own caches is not used
-            Assert.Throws<InvalidOperationException>(()=>new ActiveSessionStore(
-                null, ts.SP, ts.IActSessionOptions, ts.ISessOptions));
+            Assert.Throws<InvalidOperationException>(() => new ActiveSessionStore(
+                null, ts.RootSP, ts.IActSessionOptions, ts.ISessOptions));
 
             //Test case: null IServiceProvider argument
             Assert.Throws<ArgumentNullException>(() => new ActiveSessionStore(
@@ -32,11 +36,11 @@ namespace ActiveSession.Tests
 
             //Test case: null IOptions<ActiveSessionOptions> argument
             Assert.Throws<ArgumentNullException>(() => new ActiveSessionStore(
-                dummy_cache.Object, ts.SP, null!, ts.ISessOptions));
+                dummy_cache.Object, ts.RootSP, null!, ts.ISessOptions));
 
             //Test case: null IOptions<SessionOptions> argument
             Assert.Throws<ArgumentNullException>(() => new ActiveSessionStore(
-                dummy_cache.Object, ts.SP, ts.IActSessionOptions, null!));
+                dummy_cache.Object, ts.RootSP, ts.IActSessionOptions, null!));
 
             //Test case: using shared cache
             using (ts.CreateStore()) {
@@ -51,7 +55,7 @@ namespace ActiveSession.Tests
             Mock<IMemoryCache> dummy_cache = new Mock<IMemoryCache>();
             ActiveSessionStore store;
             ActiveSessionStoreStats? stats;
-            ts =new ConstructorTestSetup(dummy_cache);
+            ts=new ConstructorTestSetup(dummy_cache);
 
             //Test case: no statistics tracking
             using (store=ts.CreateStore()) {
@@ -72,6 +76,102 @@ namespace ActiveSession.Tests
             }
         }
 
+        [Fact]
+        public void CreateOrFetchSessionTest()
+        {
+            CreateFetchTestSetup ts;
+            ActiveSessionStore store;
+            IActiveSession session;
+            ts= new CreateFetchTestSetup();
+            using (store=ts.CreateStore()) {
+                session = store.FetchOrCreateSession(ts.StubSession.Object, null);
+            }
+
+        }
+
+        class MockedCache
+        {
+            public readonly Mock<IMemoryCache> CacheMock;
+            public readonly Mock<ICacheEntry> EntryMock;
+            Boolean _isEntryStored;
+            Object? _key;
+            Object? _value;
+            DateTimeOffset? _absoluteExpiration;
+            TimeSpan? _absoluteExpirationRelativeToNow;
+            IList<IChangeToken> _expirationTokens = new List<IChangeToken>();
+            readonly IList<PostEvictionCallbackRegistration> _postEvictionCallbacks = new List<PostEvictionCallbackRegistration>();
+            CacheItemPriority _priority=CacheItemPriority.Normal;
+            Int64? _size;
+            TimeSpan? _slidingExpiration;
+
+            public MockedCache()
+            {
+                CacheMock=new Mock<IMemoryCache>();
+                EntryMock=new Mock<ICacheEntry>();
+
+                CacheMock.Setup(s => s.CreateEntry(It.IsAny<Object>()))
+                    .Callback((Object key) => { ClearStoredEntry(); _key=key; })
+                    .Returns(EntryMock.Object);
+                CacheMock.Setup(s => s.TryGetValue(It.IsAny<Object>(), out It.Ref<Object>.IsAny))
+                    .Callback((Object key, ref Object value) => { value=_value!; })
+                    .Returns(_isEntryStored&&Key.Equals(_key));
+                CacheMock.Setup(s => s.Remove(It.IsAny<Object>()))
+                    .Callback((Object Key) => { CheckKey(Key); ClearStoredEntry(); });
+
+                EntryMock.Setup(s => s.Dispose()).Callback(() => { if (!_isEntryStored&&EntryMock.Object.Value!=null) StoreEntry(); });
+                EntryMock.SetupProperty(s => s.Value);
+                EntryMock.SetupProperty(s => s.AbsoluteExpiration);
+                EntryMock.SetupProperty(s => s.AbsoluteExpirationRelativeToNow);
+                EntryMock.SetupGet(s => s.ExpirationTokens).Returns(_expirationTokens);
+                EntryMock.SetupGet(s=>s.PostEvictionCallbacks).Returns(_postEvictionCallbacks);
+                EntryMock.SetupProperty(s=>s.Priority).Object.Value = CacheItemPriority.Normal;
+                EntryMock.SetupProperty(s => s.Size);
+                EntryMock.SetupProperty(s => s.SlidingExpiration);
+            }
+
+            void CheckKey(Object Key)
+            {
+                if (Key==null) throw new ArgumentNullException(nameof(Key));
+                if (_isEntryStored&&!Key.Equals(_key))
+                    throw new InvalidOperationException("Mock limitation: cannot operate with another entry while one is stored already.");
+            }
+
+            void ClearStoredEntry()
+            {
+                _isEntryStored=false;
+                _value=null;
+                _absoluteExpiration=null;
+                _absoluteExpirationRelativeToNow=null;
+                _expirationTokens.Clear();
+                _postEvictionCallbacks.Clear();
+                _priority = CacheItemPriority.Normal;
+                _size=null;
+                _slidingExpiration=null;
+            }
+
+            void StoreEntry()
+            {
+                _value=EntryMock.Object.Value;
+                _absoluteExpiration=EntryMock.Object.AbsoluteExpiration;
+                _absoluteExpirationRelativeToNow=EntryMock.Object.AbsoluteExpirationRelativeToNow;
+                _priority=EntryMock.Object.Priority;
+                _size=EntryMock.Object.Size;
+                _slidingExpiration=EntryMock.Object.SlidingExpiration;
+                _isEntryStored =true;
+            }
+
+            //Tracked ICacheEntry properties
+            public Object? Value { get => _value; }
+            public DateTimeOffset? AbsoluteExpiration { get => _absoluteExpiration; }
+            public TimeSpan? AbsoluteExpirationRelativeToNow { get => _absoluteExpirationRelativeToNow; }
+            // TODO ExpirationTokens are not tracked
+            public Object Key { get => _key!; }
+            public IList<PostEvictionCallbackRegistration> PostEvictionCallbacks { get => _postEvictionCallbacks; } 
+            public CacheItemPriority Priority { get => _priority; }
+            public Int64? Size { get => _size; }
+            public TimeSpan? SlidingExpiration { get => _slidingExpiration; }
+        }
+
         class ConstructorTestSetup
         {
             public ActiveSessionOptions ActSessOptions = new ActiveSessionOptions();
@@ -81,13 +181,13 @@ namespace ActiveSession.Tests
             public readonly Mock<IMemoryCache>? MockCache;
             public readonly Mock<ILoggerFactory> MockLogerFactory;
             public Expression<Func<ILoggerFactory, ILogger>> LoggerCreateExpression = s => s.CreateLogger(LOGGING_CATEGORY_NAME);
-            public IServiceProvider SP { get { return _fakeServiceProvider.Object; } }
+            public IServiceProvider RootSP { get { return _fakeRootServiceProvider.Object; } }
 
-            protected readonly Mock<IServiceProvider> _fakeServiceProvider;
+            protected readonly Mock<IServiceProvider> _fakeRootServiceProvider;
 
             public ConstructorTestSetup(Mock<IMemoryCache>? MockCache)
             {
-                _fakeServiceProvider=new Mock<IServiceProvider>();
+                _fakeRootServiceProvider=new Mock<IServiceProvider>();
                 MockLogerFactory = new Mock<ILoggerFactory>();
                 MockLogerFactory.Setup(LoggerCreateExpression).Returns<ILogger>(null);
                 this.MockCache=MockCache;
@@ -100,11 +200,62 @@ namespace ActiveSession.Tests
                 IMemoryCache? cache = MockCache?.Object;
                 return new ActiveSessionStore(
                     cache,
-                    _fakeServiceProvider.Object,
+                    _fakeRootServiceProvider.Object,
                     IActSessionOptions,
                     ISessOptions,
                     MockLogerFactory.Object);
             }
         }
+
+        class MockedCaheTestSetup : ConstructorTestSetup
+        {
+            public readonly MockedCache Cache;
+            public const String TEST_SESSION_ID = "TestSessionId";
+
+
+            protected MockedCaheTestSetup(MockedCache Cache) : base(Cache.CacheMock)
+            {
+                this.Cache=Cache;
+            }
+
+
+        }
+
+        class CreateFetchTestSetup : MockedCaheTestSetup
+        {
+            public readonly Mock<ISession> StubSession;
+            public Boolean ScopeDisposed { get; private set; }
+            public IServiceProvider ScopeServiceProvider { 
+                get {
+                    if (ScopeDisposed) throw new ObjectDisposedException("IServiceScope");
+                    return _fakeSessionServiceProvider.Object; 
+                } 
+            }
+
+            protected  readonly Mock<IServiceProvider> _fakeSessionServiceProvider;
+
+            readonly Mock<IServiceScopeFactory> _fakeScopeFactory;
+            readonly Mock<IServiceScope> _fakeServiceScope;
+            readonly Expression<Action<IServiceScope>> _disposeExpression = s=>s.Dispose();
+
+
+            public CreateFetchTestSetup() : base(new MockedCache())
+            {
+                StubSession=new Mock<ISession>();
+                StubSession.SetupGet(s => s.Id).Returns(TEST_SESSION_ID);
+                _fakeSessionServiceProvider = new Mock<IServiceProvider>();
+                _fakeServiceScope=new Mock<IServiceScope>();
+                _fakeServiceScope.SetupGet(s => s.ServiceProvider).Returns(ScopeServiceProvider); 
+                _fakeServiceScope.Setup(_disposeExpression).Callback(() => { ScopeDisposed=true; });
+                _fakeScopeFactory=new Mock<IServiceScopeFactory>();
+                _fakeScopeFactory.Setup(s => s.CreateScope())
+                    .Callback(() => ScopeDisposed=false)
+                    .Returns(_fakeServiceScope.Object);
+                _fakeRootServiceProvider.Setup(s => s.GetService(typeof(IServiceScopeFactory)))
+                    .Returns(_fakeScopeFactory.Object);
+            }
+
+        }
+
     }
 }
