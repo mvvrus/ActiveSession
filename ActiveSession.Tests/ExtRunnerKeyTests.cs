23f44e50617c6bc9ffef79274a3a9152f4b0c7c7
mvvrus <vmurav@yandex.ru>
2024-07-26 18:19:18 +0300

    Make additional changes for ExtRunnerKey class.
    Add IsForSession method, correct XML documentation, add more tests.

--- ActiveSession.Tests/ExtRunnerKeyTests.cs
+++ ActiveSession.Tests/ExtRunnerKeyTests.cs
@@ -28,5 +28,40 @@ namespace ActiveSession.Tests
             Assert.False(ExtRunnerKey.TryParse("19-{AS_ID}", out value));
             Assert.False(ExtRunnerKey.TryParse("42-19-", out value));
         }
+
+        [Fact]
+        public void ImplicitConversionTest()
+        {
+            ExtRunnerKey sample = new ExtRunnerKey(RunnerNumber: 42, Generation: 19, ActiveSessionId: AS_ID), value;
+            value=(42, AS_ID, 19);
+            Assert.Equal(sample, value);
+
+            Mock<IActiveSession> stub_session=new Mock<IActiveSession>();
+            stub_session.SetupGet(s=>s.Id).Returns(AS_ID);
+            stub_session.SetupGet(s => s.Generation).Returns(19);
+            value= (stub_session.Object, 42);
+            Assert.Equal(sample, value);
+        }
+
+        [Fact]
+        public void IsForSessionTest()
+        {
+            ExtRunnerKey sample;
+            Mock<IActiveSession> stub_session = new Mock<IActiveSession>();
+            stub_session.SetupGet(s => s.Id).Returns(AS_ID);
+            stub_session.SetupGet(s => s.Generation).Returns(19);
+
+            sample= new ExtRunnerKey(RunnerNumber: 42, Generation: 19, ActiveSessionId: AS_ID);
+            Assert.True(sample.IsForSession(stub_session.Object));
+
+            sample= new ExtRunnerKey(RunnerNumber: 1, Generation: 19, ActiveSessionId: AS_ID);
+            Assert.True(sample.IsForSession(stub_session.Object));
+
+            sample= new ExtRunnerKey(RunnerNumber: 42, Generation: 1, ActiveSessionId: AS_ID);
+            Assert.False(sample.IsForSession(stub_session.Object));
+
+            sample= new ExtRunnerKey(RunnerNumber: 42, Generation: 19, ActiveSessionId: Guid.NewGuid().ToString());
+            Assert.False(sample.IsForSession(stub_session.Object));
+        }
     }
 }
--- MVVrus.AspNetCore.ActiveSession/ExtRunnerKey.cs
+++ MVVrus.AspNetCore.ActiveSession/ExtRunnerKey.cs
@@ -6,32 +6,41 @@ using System.Web;
 namespace MVVrus.AspNetCore.ActiveSession
 {
     /// <summary>
-    /// The type to be used to pass a runner identifier into a code external to an ActiveSession library.
+    /// This struct is intended to pass a runner identifier into a code external to an ActiveSession library (e.g as a part of URL).
     /// </summary>
     /// <param name="RunnerNumber">A number assigned to the runner within the Active Session it belongs to.</param>
-    /// <param name="ActiveSessionId">TODO</param>
-    /// <param name="Generation">Generation property value of the ActiveSession</param>
+    /// <param name="ActiveSessionId">
+    /// An identifier (<see cref="IActiveSession.Id">Id</see> property value) of the Active Session the runner belongs to.
+    /// </param>
+    /// <param name="Generation">
+    /// The <see cref="IActiveSession.Generation">Generation</see> property value of the ActiveSession the runner belongs to.
+    /// </param>
     /// <remarks>
-    /// A purpose of this type is to avoid possible confusions if an Active session was changed between HTTP requests.
-    ///<br/> The string represetation of an identifier (returned by its <see cref="ToString"/> method) has the form 
-    /// <br/>"{<see cref="Generation"/>-{<see cref="RunnerNumber"/>"
+    /// A purpose of this type is to avoid a mess if an ActiveSession assigned to different HTTP requests differs.
+    ///<br/> The string represetation of an identifier (returned by its <see cref="ToString"/> method
+    /// and parseable by its static <see cref="TryParse(string, out ExtRunnerKey)">TryParse</see> method) has the form 
+    /// <br/><inheritdoc cref="ToString" path='/returns/format' />".
     /// </remarks>
     public record struct ExtRunnerKey(Int32 RunnerNumber, String ActiveSessionId, Int32 Generation)
     {
 
         /// <summary>
-        /// Converts a tuple of runner number(<see cref="int"/>), ActiveSession identifier (<see cref="string"/>) and genration number (<see cref="int"/>) to an ExtRunnerKey value.
+        /// Converts a tuple of a runner number(<see cref="int"/>), an ActiveSession identifier (<see cref="string"/>) 
+        /// and a genration number (<see cref="int"/>) to an ExtRunnerKey value.
         /// </summary>
         /// <param name="Value">The tuple to be converted.</param>
+        /// <returns>The result of the conversion.</returns>
         public static implicit operator  ExtRunnerKey((Int32 RunnerNumber, String ActiveSessionId, Int32 Generation) Value)
         {
             return new ExtRunnerKey(Value.RunnerNumber, Value.ActiveSessionId, Value.Generation);
         }
 
         /// <summary>
-        /// Converts a tuple of ActiveSession reference(IActiveSession) and runner number (int) to an ExtRunnerKey value.
+        /// Converts a tuple of an ActiveSession reference(<see cref="IActiveSession"/>) and a runner number (<see cref="int"/>) 
+        /// to an ExtRunnerKey value.
         /// </summary>
         /// <param name="Value">The tuple to be converted.</param>
+        /// <returns>The result of the conversion.</returns>
         public static implicit operator ExtRunnerKey((IActiveSession ActiveSession, Int32 RunnerNumber) Value)
         {
             return (Value.RunnerNumber, Value.ActiveSession.Id, Value.ActiveSession.Generation);
@@ -41,7 +50,7 @@ namespace MVVrus.AspNetCore.ActiveSession
         /// Make string representation of this instance.
         /// </summary>
         /// <returns>
-        /// String "{<see cref="Generation"/>}-{<see cref="RunnerNumber"/>-<see cref="ActiveSessionId"/>}" if the instance has a value
+        /// A string of form <format>"{<see cref="RunnerNumber"/>}-{<see cref="Generation"/>-<see cref="ActiveSessionId"/>}"</format>>
         /// </returns>
         public override String ToString()
         {
@@ -49,15 +58,30 @@ namespace MVVrus.AspNetCore.ActiveSession
         }
 
         /// <summary>
-        /// A regular expression pattern used to parse the string representation of a value of this type
+        /// Check if this instnce was issued for the ActiveSession passed as the parameter.
+        /// </summary>
+        /// <param name="Session">The ActiveSession to check affiliation with.</param>
+        /// <returns>
+        /// <see langword="true"/> if this instance belongs to an ActiveSession passed by <paramref name="Session"/> parameter,
+        /// <see langword="false"/> otherwise
+        /// </returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Boolean IsForSession(IActiveSession Session)
+        {
+           if(Session == null) throw new ArgumentNullException(nameof(Session));
+           return Session.Id==ActiveSessionId && Session.Generation==Generation;
+        }
+
+        /// <summary>
+        /// A regular expression pattern used to parse the string representation of a value of this type.
         /// </summary>
         public const String RunnerKeyTemplate = @"^(\d+)-(\d+)-(.+)$";
 
         /// <summary>
-        /// A static method that can be used for parsing the string representation of a value of this type
+        /// A static method that can be used for parsing a string representation of a value of this type.
         /// </summary>
         /// <param name="Source">The string to parse.</param>
-        /// <param name="ExtRunnerKey">Parse result if the parsing operation was successful.</param>
+        /// <param name="ExtRunnerKey">The parse result if the parsing operation was successful.</param>
         /// <returns>A value showing was the parsing operation successful.</returns>
         /// <exception cref="ArgumentNullException"></exception>
         public static Boolean TryParse(String Source, out ExtRunnerKey ExtRunnerKey)
@@ -83,7 +107,8 @@ namespace MVVrus.AspNetCore.ActiveSession
         /// <inheritdoc cref="TryParse(string, out ExtRunnerKey)" path='/param[@name="Source"]'/>
         /// </param>
         /// <returns>
-        /// <inheritdoc cref="TryParse(string, out ExtRunnerKey)" path='/param[@name="RunnerKey]'/>
+        /// <inheritdoc cref="TryParse(string, out ExtRunnerKey)" path='/param[@name="ExtRunnerKey"]'/>
+        /// <br/>Otherwise a <see cref="FormatException"/> will be thrown.
         /// </returns>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="FormatException"></exception>
