01b633184ec03a2becab66ec6e1642b0e1999f8c
mvvrus <vmurav@yandex.ru>
2024-11-06 05:03:04 +0300

    Implement Active Session suffix configuration classes.

--- ActiveSession.Tests/MiddlewareFilterSourcesTests.cs
+++ ActiveSession.Tests/MiddlewareFilterSourcesTests.cs
@@ -35,6 +35,34 @@ namespace ActiveSession.Tests
             Assert.Equal(DEFAULT_ORDER, order);
         }
 
+        const String SUFFIX1 = "1";
+
+        [Fact]
+        public void PredicateWithSuffixFilterSource()
+        {
+            Boolean was_mapped;
+            String? session_suffix;
+            Int32 order;
+
+            Func<HttpContext, Boolean> predicate = context => { return context.Request.Path.StartsWithSegments(PATH1); };
+            var source = new PredicateWithSuffixFilterSource(predicate,SUFFIX1);
+            var filter = source.Create(DEFAULT_ORDER);
+            FakeHttpContext fake_context = new FakeHttpContext();
+            Assert.NotNull(filter);
+            Assert.Equal(DEFAULT_ORDER, filter.MinOrder);
+            fake_context.SetPath(PATH1);
+            (was_mapped, session_suffix, order)=filter.Apply(fake_context.Context);
+            Assert.True(was_mapped);
+            Assert.Equal(SUFFIX1,session_suffix);
+            Assert.Equal(DEFAULT_ORDER, order);
+
+            fake_context.SetPath(PATH2);
+            (was_mapped, session_suffix, order)=filter.Apply(fake_context.Context);
+            Assert.False(was_mapped);
+            Assert.Null(session_suffix);
+            Assert.Equal(DEFAULT_ORDER, order);
+        }
+
         class FakeHttpContext
         {
             public HttpContext Context { get => _mockContext.Object; }
---
+++ MVVrus.AspNetCore.ActiveSession/Internal/PredicateWithSuffixFilterSource.cs
@@ -0,0 +1,44 @@
+﻿internal class PredicateWithSuffixFilterSource : IMiddlewareFilterSource
+{
+    Func<HttpContext, Boolean> _predicate;
+    String _suffix;
+
+    public PredicateWithSuffixFilterSource(Func<HttpContext, Boolean> Predicate, String Suffix)
+    {
+        _predicate=Predicate;
+        _suffix=Suffix;
+    }
+
+    public Boolean HasSuffix => true;
+    public Boolean IsGroupable => false;
+    public Boolean Group(IMiddlewareFilter GroupFilter) { throw new InvalidOperationException(); }
+
+    internal Func<HttpContext, Boolean> Predicate { get => _predicate; }
+
+    public IMiddlewareFilter Create(Int32 Order)
+    {
+        return new MiddlewareFilter(_predicate, _suffix, Order);
+    }
+
+    class MiddlewareFilter : IMiddlewareFilter
+    {
+        Func<HttpContext, Boolean> _filter;
+        String _suffix;
+        Int32 _order;
+
+        public MiddlewareFilter(Func<HttpContext, Boolean> Filter, String Suffix, Int32 Order)
+        {
+            _filter=Filter;
+            _suffix = Suffix;
+            _order=Order;
+        }
+        public Int32 MinOrder => _order;
+
+        public (Boolean WasMapped, String? SessionSuffix, Int32 Order) Apply(HttpContext Context)
+        {
+            Boolean passed = _filter(Context);
+            return (passed, passed?_suffix:null, _order);
+        }
+    }
+
+}
