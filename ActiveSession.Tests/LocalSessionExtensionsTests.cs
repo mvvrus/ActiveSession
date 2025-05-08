7b5009d14997c948a48090016c6484c0debdd770
mvvrus <vmurav@yandex.ru>
2025-05-08 05:19:51 +0300

    Add TakeOwnership extension method for ILocfaSession

---
+++ ActiveSession.Tests/LocalSessionExtensionsTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveSession.Tests
+{
+    public class LocalSessionExtensionsTests
+    {
+        //Test case: TakeOwnership happy path
+        [Fact]
+        public void TakeOwnershipNormal()
+        {
+            using (TestSetup ts=new TestSetup() { IsAvailable=true }) {
+                TestDisposable td = new TestDisposable();
+                ts.LocalSession.TakeOwnership(td);
+                CancellationToken token = ts.LocalSession.CompletionToken;
+                ts.Cancel();
+                Assert.True(token.IsCancellationRequested);
+                Assert.True(td.Disposed);
+            }
+        }
+
+        //Test case: TakeOwnership premature cleanup of the associated object
+        [Fact]
+        public void TakeOwnershipPrematureCleanup()
+        {
+            using(TestSetup ts = new TestSetup() { IsAvailable=true }) {
+                TestDisposable td = new TestDisposable();
+                IDisposable reg=ts.LocalSession.TakeOwnership(td);
+                CancellationToken token = ts.LocalSession.CompletionToken;
+                reg.Dispose();
+                ts.Cancel();
+                Assert.True(token.IsCancellationRequested);
+                Assert.True(td.Disposed);
+            }
+        }
+
+        //Test case: TakeOwnership on unavailable LocalSession
+        [Fact]
+        public void TakeOwnershipUnavailable()
+        {
+            using(TestSetup ts = new TestSetup() { IsAvailable=false }) {
+                TestDisposable td = new TestDisposable();
+                Assert.Throws<InvalidOperationException>(()=>ts.LocalSession.TakeOwnership(td));
+            }
+        }
+
+        //Test case: TakeOwnership happy path and then cleanup
+        [Fact]
+        public void TakeOwnershipLateCleanup()
+        {
+            using(TestSetup ts = new TestSetup() { IsAvailable=true }) {
+                TestDisposable td = new TestDisposable();
+                IDisposable reg = ts.LocalSession.TakeOwnership(td);
+                CancellationToken token = ts.LocalSession.CompletionToken;
+                ts.Cancel();
+                reg.Dispose();
+                Assert.True(token.IsCancellationRequested);
+                Assert.True(td.Disposed);
+            }
+        }
+
+        class TestDisposable : IDisposable
+        {
+            public Boolean Disposed { get; private set; }
+            public void Dispose()
+            {
+                if(Disposed) throw new ObjectDisposedException(GetType().FullName);
+                Disposed=true;
+            }
+        }
+
+        class TestSetup : IDisposable
+        {
+            Mock<ILocalSession> _localSessionMock;
+            CancellationTokenSource _cts;
+
+            public ILocalSession LocalSession => _localSessionMock.Object;
+            public Boolean IsAvailable { get; set; }
+
+            public TestSetup()
+            {
+                _localSessionMock = new Mock<ILocalSession>(MockBehavior.Strict);
+                _localSessionMock.SetupGet(s => s.IsAvailable).Returns(() => IsAvailable);
+                _cts = new CancellationTokenSource();
+                _localSessionMock.SetupGet(s => s.CompletionToken).Returns(() => _cts.Token);
+            }
+
+            public void Cancel()
+            {
+                _cts.Cancel();
+                _cts.Dispose();
+                _cts=new CancellationTokenSource();
+            }
+
+            public void Dispose()
+            {
+                _cts.Dispose();
+            }
+        }
+    }
+}
---
+++ MVVrus.AspNetCore.ActiveSession/LocalSessionExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace MVVrus.AspNetCore.ActiveSession
+{
+    /// <summary>
+    /// This static class is a container for extension methods for <see cref="ILocalSession"/> interface.
+    /// </summary>
+    public static class LocalSessionExtensions
+    {
+
+        /// <summary>
+        /// Pass responsibility of disposing the disposable object passed to an object implementing the <see cref="ILocalSession"/> interface.
+        /// </summary>
+        /// <param name="Session">The <see cref="ILocalSession"/> implementation to which the responsibility is to be passed.</param>
+        /// <param name="Disposable">The disposable object passed.</param>
+        /// <returns>The registration object. Dispose it to dispose the object passed and to recall the responsibility.</returns>
+        /// <exception cref="InvalidOperationException">Raised if an attempt to pass reponsibility to an unavalable session was detected.</exception>
+        public static IDisposable TakeOwnership(this ILocalSession Session, IDisposable Disposable)
+        {
+            if(Session==null || !Session.IsAvailable) throw new InvalidOperationException("Active session or group is not available.");
+            return new Registration(
+                Session.CompletionToken, Disposable);
+        }
+
+        class Registration: IDisposable
+        {
+            CancellationToken _token;
+            CancellationTokenRegistration _registration;
+            IDisposable? _disposable;
+
+            public Registration(CancellationToken Token, IDisposable Disposable)
+            {
+                _disposable = Disposable??throw new ArgumentNullException(nameof(Disposable));
+                _token = Token;
+                _registration = Token.Register(static r => { Registration.DisposeRegistration((Registration)r!); }, this);
+            }
+
+            public void Dispose()
+            {
+                DisposeRegistration(this);
+            }
+
+            static void DisposeRegistration(Registration Registration)
+            {
+                IDisposable? disposable = Interlocked.Exchange(ref Registration._disposable, null);
+                if(disposable != null) Registration._registration.Dispose();
+                disposable?.Dispose();
+            }
+        }
+    }
+}
